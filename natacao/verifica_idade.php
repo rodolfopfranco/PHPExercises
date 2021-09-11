2580b79467a19cf26bd0e3d4348c594adb9e237b
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-09-11 18:01:33 -0300

    Basic security data validation

--- natacao/verifica_idade.php
+++ natacao/verifica_idade.php
@@ -18,6 +18,26 @@
     var_dump($idade);
     echo '<br /><br />Resultado:<br/>';
 
+    //Validação das informações:
+    if(empty($nome)){
+        echo 'O nome não pode ser vazio';
+        return;
+    }
+    if(strlen($nome)<3){
+        echo 'Nome deve ter mais de 3 caracteres';
+        return;
+    }
+    if(strlen($nome)>40){
+        echo 'Nome muito extenso';
+        return;
+    }
+    if(!is_numeric($idade)){
+        echo 'A idade precisa ser um número';
+        return;
+    }
+
+
+//Prints:
     if($idade>= 6 && $idade <= 12){
         //Concatenação:
         echo"Atleta ".$nome." compete na categoria ".$categorias[0];
