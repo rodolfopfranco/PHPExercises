97396028f560e27a6c6a1a3716ff1dd849f48dc9
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-09-18 17:38:04 -0300

    Methods, private, getters

--- orientacaoObjeto/ContaBancaria.php
+++ orientacaoObjeto/ContaBancaria.php
@@ -3,14 +3,46 @@
 class ContaBancaria{
     //modificadores de acesso:
     //public - private - protected
-    public $nomeTitular;
-    public $numeroAgencia;
-    public $numeroConta;
-    public $saldo;
-    public $banco;
+    private $nomeTitular;
+    private $numeroAgencia;
+    private $numeroConta;
+    private $saldo;
+    private $banco;
+
+    //Construtor:
+    public function __construct($banco, $nomeTitular, $numeroAgencia, $numeroConta, $saldo){
+        $this->banco = $banco;
+        $this->nomeTitular = $nomeTitular;
+        $this->numeroAgencia = $numeroAgencia;
+        $this->saldo = $saldo;
+        $this->numeroConta = $numeroConta;
+        echo "Objeto criado";
+        //pode ser passado os atributos, criar banco, etc
+    }
+
+    //Métodos:
+    public function obterSaldo(){
+        return $this->saldo;
+    }
+
+    public function depositar($valor){
+        $this->saldo+=$valor;
+    }
+
+    public function sacar($valor){
+        $this->saldo -= $valor;
+    }
 
 }
 
 //Instanciando:
-$conta = new ContaBancaria();
+$conta = new ContaBancaria(
+    'Banco do Brasil',
+    'Gustavo Fraga',
+    '8244',
+    '12345',
+    300.00
+);
+
+echo $conta->obterSaldo();
 ?>
