1727a33aced815cbef6ed75b3bba0b3f58a28374
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-09-18 16:45:51 -0300

    Creating an object with public modifier

---
+++ orientacaoObjeto/ContaBancaria.php
@@ -0,0 +1,16 @@
+<?php
+//Objeto criado:
+class ContaBancaria{
+    //modificadores de acesso:
+    //public - private - protected
+    public $nomeTitular;
+    public $numeroAgencia;
+    public $numeroConta;
+    public $saldo;
+    public $banco;
+
+}
+
+//Instanciando:
+$conta = new ContaBancaria();
+?>
